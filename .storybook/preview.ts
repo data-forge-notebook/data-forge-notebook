9bbfa506304b9455a8a3814ac223cedbf50e5cff
Ashley Davis <ashley@codecapers.com.au>
2022-03-19 14:04:05 +1000

    Imported styles to work in Storybook.

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,1 +1,7 @@
+import "../node_modules/normalize.css/normalize.css";
+import "../src/testbed/styles/base.css";
+import "../node_modules/@blueprintjs/icons/lib/css/blueprint-icons.css";
+import "../node_modules/@blueprintjs/core/lib/css/blueprint.css";
+import "../src/testbed/styles/index.css";
+
 import "../src/testbed/services/plugin-repository";
