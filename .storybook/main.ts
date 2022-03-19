8f28fd56ee9a3185157cadfcb0a917a15bbf99b2
Ashley Davis <ashley@codecapers.com.au>
2022-03-19 10:58:00 +1000

    Cleaned up Storybook configuration.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,15 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/react"
-}
+    "stories": [
+      "../src/**/*.stories.mdx",
+      "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    ],
+    "addons": [
+      "@storybook/addon-links",
+      "@storybook/addon-essentials",
+      "@storybook/addon-interactions"
+    ],
+    "framework": "@storybook/react",
+    "core": {
+      "builder": "webpack5"
+    }
+  }
