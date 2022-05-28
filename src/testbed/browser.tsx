e9971153f12ce61e47d1b5ed8a113f55fd81df59
Ashley Davis <ashley@codecapers.com.au>
2022-05-29 08:25:44 +1000

    Improved text at the top of the browser testbed.

--- src/testbed/browser.tsx
+++ src/testbed/browser.tsx
@@ -7,21 +7,18 @@ function App() {
     return (
         <>
             <div className="centered-container mb-30">
-                <h1>Data-Forge Notebook: Browser testing environment</h1>
+                <h1>Data-Forge Notebook v2: Browser testing environment</h1>
                 <p>
                     This is the browser testing environment for DFN v2.
-                    This interface also runs on the desktop using Electron.
+                    This user interface also runs on the desktop using Electron.
                     What you see here is a hard coded JavaScript notebook.
-                    You can't edit this yet, but it is starting to look like a real notebok!
+                    You can't edit this yet, but full editing and code evaluation capabiltites matching <a target="_blank" href="https://www.data-forge-notebook.com/">DFN v1</a> aren't far away!
                 </p>
                 <p className="mt-2">
-                    Checkout <a target="_blank" href="https://github.com/data-forge-notebook/editor-core/">the open source code repo</a> to follow development of DFN v2.
+                    <a target="_blank" href="https://github.com/data-forge-notebook/editor-core/">The code for DFN v2 is open source</a>.
                 </p>
                 <p>
-                    To edit and evaluate JavaScript notebooks, use <a target="_blank" href="https://www.data-forge-notebook.com/">use Data-Forge Notebook v1</a>.
-                </p>
-                <p>
-                    Follow <a target="_blank" href="https://twitter.com/codecapers">the developer on Twitter</a> for more frequent updates.
+                    <a target="_blank" href="https://twitter.com/codecapers">Follow on Twitter</a> for updates.
                 </p>
                 
             </div>
