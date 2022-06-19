0376226e31966529962be39846562550a7e5d63c
Ashley Davis <ashley@codecapers.com.au>
2022-06-19 19:01:16 +1000

    Fix for Storybook so that vizualization plugins can be loaded into the bundle.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,15 +1,24 @@
 module.exports = {
     "stories": [
-      "../src/**/*.stories.mdx",
-      "../src/**/*.stories.@(js|jsx|ts|tsx)"
+        "../src/**/*.stories.mdx",
+        "../src/**/*.stories.@(js|jsx|ts|tsx)"
     ],
     "addons": [
-      "@storybook/addon-links",
-      "@storybook/addon-essentials",
-      "@storybook/addon-interactions"
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions"
     ],
     "framework": "@storybook/react",
     "core": {
-      "builder": "webpack5"
-    }
-  }
+        "builder": "webpack5"
+    },
+    // https://storybook.js.org/docs/react/builders/webpack
+    "webpackFinal": async (config, { configType }) => {
+        config.module.rules.push({
+            test: /\.txt$/,
+            type: 'asset/source',
+        });
+
+        return config;
+    },
+}
