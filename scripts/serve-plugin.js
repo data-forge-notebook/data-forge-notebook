e983b7f4a1f5f4a8c40cd5a3850107dfe08a31dc
Ashley Davis <ashley@codecapers.com.au>
2023-10-02 16:51:34 +1000

    Spelling.

--- scripts/serve-plugin.js
+++ scripts/serve-plugin.js
@@ -24,7 +24,7 @@ const path = require("path");
 const { exec } = require("child_process");
 
 //
-// Servers a named plugiun.
+// Serves a named plugin.
 //
 function servePlugin(pluginName, portNo, outputDir) {
     fs.ensureDirSync(outputDir);
