e4519cd3df234bc8e414309276f4e6b649e1c68f
Ashley Davis <ashley@codecapers.com.au>
2023-10-28 17:24:10 +1000

    Refined formats for the table plugin.

--- plugins/table/src/table.tsx
+++ plugins/table/src/table.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import typy from "typy";
 
 //
 // Represents a table.
@@ -7,7 +8,7 @@ export interface ITable {
     //
     // Columns to display (fields in the data) and the order in which to display them.
     //
-    columnNames: string[]; 
+    columns: string[]; 
 
     //
     // The data to display in the table.
@@ -19,16 +20,58 @@ export interface ITableProps {
     //
     // Data to be displayed.
     //
-    table?: ITable;
+    table?: ITable | any;
 }
 
 export class Table extends React.Component<ITableProps, {}> {
 
     render () {
-        if (!this.props.table) {
+
+        let table = this.props.table;
+        if (!table) {
             return (
                 <div>Table data not provided</div>
             );
+        }
+
+        if (typy(table).isObject) {
+            if (!Array.isArray(table.columns) || !Array.isArray(table.rows)) {
+                //
+                // Don't have correct fields for proper table format.
+                // Just convert the object to a table.
+                //
+                table = {
+                    columns: ["Field", "Value"],
+                    rows: Object.entries(table),
+                };
+            }
+        }
+        else if (typy(table).isArray) {
+            if (table.length === 0) {
+                table = {
+                    columns: [],
+                    rows: [],
+                };
+            }
+            else if (typy(table[0]).isObject) {
+                const columns = Object.keys(table[0]);
+                table = {
+                    columns: columns,
+                    rows: table.map((row: any) => columns.map((column) => row[column])),
+                };
+            }
+            else if (typy(table[0]).isArray) {
+                table = {
+                    columns: table[0],
+                    rows: table.slice(1),
+                };
+            }
+            else {
+                return <div>Bad table data format</div>
+            }
+        }
+        else {
+            return <div>Bad table data format</div>
         }
 
         return (
@@ -43,16 +86,16 @@ export class Table extends React.Component<ITableProps, {}> {
                 <table>
                     <thead>
                         <tr>
-                            {this.props.table.columnNames.map((columnName, i) => 
-                                <th key={i}>{columnName}</th>
+                            {table.columns.map((column: string, i: number) => 
+                                <th key={i}>{column}</th>
                             )}
                         </tr>
                     </thead>
                     <tbody>
-                        {this.props.table.rows.map((row, i) => 
+                        {table.rows.map((row: any[], i: number) => 
                             <tr key={i}>
-                                {this.props.table!.columnNames.map((columnName, i) =>
-                                    <td key={i}>{row[columnName]}</td>
+                                {table.columns.map((column: string, i: number) =>
+                                    <td key={i}>{row[i]}</td>
                                 )}
                             </tr>
                         )}
