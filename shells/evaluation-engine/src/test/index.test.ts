ca6598788ff17c19f451fa5d682c991bc5c9b20b
Ashley Davis <ashley@codecapers.com.au>
2023-11-11 16:47:16 +1000

    Updated evaluation engine tests.

--- shells/evaluation-engine/src/test/index.test.ts
+++ shells/evaluation-engine/src/test/index.test.ts
@@ -21,17 +21,16 @@ describe("evaluation engine shell", () => {
         const response = await request().get("/status");
         expect(response.status).toBe(200);
         expect(response.body).toEqual({ 
-            numEvaluations: 0,
-            notebookIds: [],
+            notebooks: [],
          });
     });
 
-    it("can evaluate notebook", async ()  => {
+    it("can install notebook", async ()  => {
 
         const notebookId = "8471e81a-844b-49df-939d-9ee4672e0bc1";
 
-        const response = await request()
-            .post("/evaluate")
+        const installResponse = await request()
+            .post("/install")
             .send({
                 "notebookId": notebookId,
                 "notebook": {
@@ -47,7 +46,7 @@ describe("evaluation engine shell", () => {
                 }
             });
 
-        expect(response.status).toBe(200);
+        expect(installResponse.status).toBe(200);
 
         let messages: any[] = [];
         
@@ -67,13 +66,114 @@ describe("evaluation engine shell", () => {
                 .send({ notebookId: notebookId });
             messages = messages.concat(messagesResponse.body.messages);
 
-            if (statusResponse.body.numEvaluations <= 0) {
+            expect(statusResponse.body.notebooks.length).toBe(1);
+
+            if (!statusResponse.body.notebooks[0].working 
+                && statusResponse.body.notebooks[0].success) {
                 // Evaluation has completed.
                 break;
             }
         }
 
+        //
+        // TODO: This breaks the next test!
+        //
+        // const stopResponse = await request()
+        //     .post("/stop-evaluation")
+        //     .send({ notebookId });
+        // expect(stopResponse.status).toBe(200);
+
+        //
+        // Remove debug messages.
+        //
+        messages = messages.filter(message => message.name !== "evaluation-event" || message.args.event !== "debug-log");
+
         expect(messages).toEqual([
+            {
+                "name": "evaluation-event",
+                "args": {
+                    "event": "notebook-install-started"
+                },
+                "notebookId": "8471e81a-844b-49df-939d-9ee4672e0bc1"
+            },
+            {
+                "name": "evaluation-event",
+                "args": {
+                    "event": "notebook-install-completed"
+                },
+                "notebookId": "8471e81a-844b-49df-939d-9ee4672e0bc1"
+            },
+        ]);
+    });
+
+    it("can evaluate notebook", async ()  => {
+
+        const notebookId = "8471e81a-844b-49df-939d-9ee4672e0bc1";
+
+        const evaluateResponse = await request()
+            .post("/evaluate")
+            .send({
+                "notebookId": notebookId,
+                "notebook": {
+                    "version": 3,
+                    "language": "javascript",
+                    "cells": [
+                        {
+                            "id": "f0627130-2e9c-11e9-9f0a-97b474081a71",
+                            "cellType": "code",
+                            "code": "console.log(\"fooey\");"
+                        }
+                    ]
+                }
+            });
+
+        expect(evaluateResponse.status).toBe(200);
+
+        let messages: any[] = [];
+        
+        while (true) {
+            await sleep(100);
+
+            //
+            // Check evaluation status.
+            //
+            const statusResponse = await request().get("/status");
+
+            //
+            // Collect messages produces by the evaluation.
+            //
+            const messagesResponse = await request()
+                .post("/messages")
+                .send({ notebookId: notebookId });
+            messages = messages.concat(messagesResponse.body.messages);
+
+            expect(statusResponse.body.notebooks.length).toBe(1);
+
+            if (!statusResponse.body.notebooks[0].working 
+                && statusResponse.body.notebooks[0].success) {
+                // Evaluation has completed.
+                break;
+            }
+        }
+
+        const stopResponse = await request()
+            .post("/stop-evaluation")
+            .send({ notebookId });
+        expect(stopResponse.status).toBe(200);
+
+        //
+        // Remove debug messages.
+        //
+        messages = messages.filter(message => message.name !== "evaluation-event" || message.args.event !== "debug-log");
+
+        expect(messages).toEqual([
+            {
+                "name": "evaluation-event",
+                "args": {
+                    "event": "notebook-eval-started"
+                },
+                "notebookId": "8471e81a-844b-49df-939d-9ee4672e0bc1"
+            },
             {
                 "name": "evaluation-event",
                 "args": {
