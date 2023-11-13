983997843010b288be9bcde850270ae88e1ccb15
Ashley Davis <ashley@codecapers.com.au>
2023-11-14 08:19:01 +1000

    Added example and test notebooks.

---
+++ notebooks/examples/example-code-module.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    aReusableFunction: function () {
+        console.log("Hello from a reusable function. You can use reuse this function across multiple notebooks.");
+    }
+};
