c46553fed910ebf3c8344ed5ec2c5065dcffb02d
Ashley Davis <ashley@codecapers.com.au>
2023-11-21 08:57:38 +1000

    Added descriptions for all example notebooks.

--- shells/electron/src/data/example-notebooks.ts
+++ shells/electron/src/data/example-notebooks.ts
@@ -1,72 +1,94 @@
 export const exampleNotebooks = [
   {
-    "file": "api-performance-test.notebook"
+    "file": "api-performance-test.notebook",
+    "description": "Conducts ongoing performance testing of a HTTP end-point or REST API."
   },
   {
-    "file": "charts.notebook"
+    "file": "charts.notebook",
+    "description": "Examples of rendering charts using Data-Forge Notebook."
   },
   {
-    "file": "cron-example.notebook"
+    "file": "cron-example.notebook",
+    "description": "Creates a scheduled task using the Cron library."
   },
   {
-    "file": "csv-file-example.notebook"
+    "file": "csv-file-example.notebook",
+    "description": "Example of loading, transforming and saving a CSV file."
   },
   {
-    "file": "file-system.notebook"
+    "file": "file-system.notebook",
+    "description": "Example of using the file system in Data-Forge Notebook"
   },
   {
-    "file": "global variables.notebook"
+    "file": "global variables.notebook",
+    "description": "Shows how to use global variables in Data-Forge Notebook."
   },
   {
     "file": "intro.notebook",
     "description": "Introductory notebook for Data-Forge Notebook, showing some of the basics."
   },
   {
-    "file": "json-file-example.notebook"
+    "file": "json-file-example.notebook",
+    "description": "Example of loading, transforming and saving a JSON file."
   },
   {
-    "file": "loading-a-code-module.notebook"
+    "file": "loading-a-code-module.notebook",
+    "description": "Shows how to load a separate JavaScript code module into your notebook."
   },
   {
-    "file": "maps.notebook"
+    "file": "maps.notebook",
+    "description": "Examples of rendering maps using Data-Forge Notebook."
   },
   {
-    "file": "math-rendering.notebook"
+    "file": "math-rendering.notebook",
+    "description": "Renders a math formula in Data-Forge Notebook."
   },
   {
-    "file": "parsing-javascript.notebook"
+    "file": "parsing-javascript.notebook",
+    "description": "Example of parsing JavaScript to an abstract syntax tree (AST) using Babel."
   },
   {
-    "file": "pattern-matching-with-z.notebook"
+    "file": "pattern-matching-with-z.notebook",
+    "description": "Shows how to match patterns with the Z library."
   },
   {
-    "file": "plot-charts.notebook"
+    "file": "plot-charts.notebook",
+    "description": "Simple example of plotting charts using Data-Forge Notebook."
   },
   {
-    "file": "plot-maps.notebook"
+    "file": "plot-maps.notebook",
+    "description": "Simple example of plotting maps using Data-Forge Notebook."
   },
   {
-    "file": "preview-data.notebook"
+    "file": "preview-data.notebook",
+    "description": "Shows a selection of ways to preview your data in Data-Forge Notebook."
   },
   {
-    "file": "random-data-values.notebook"
+    "file": "random-data-values.notebook",
+    "description": "Calculates a bunch of random numbers."
   },
   {
-    "file": "read-file-stream.notebook"
+    "file": "read-file-stream.notebook",
+    "description": "Pipes a file to standard output using Node.js streams."
   },
   {
-    "file": "read-write-file-stream.notebook"
+    "file": "read-write-file-stream.notebook",
+    "description": "Copies a file using Node.js streams."
   },
   {
-    "file": "render-html.notebook"
+    "file": "render-html.notebook",
+    "description": "Shows how to render HTML."
   },
   {
-    "file": "rest-api-example.notebook"
+    "file": "rest-api-example.notebook",
+    "description": "Shows how to call a REST API from Data-Forge Notebook."
   },
   {
-    "file": "scraping-demo.notebook"
+    "file": "scraping-demo.notebook",
+    "description": "Scrapes data from a web page."
   },
   {
-    "file": "viz.notebook"
+    "file": "viz.notebook",
+    "description": "Shows various ways to visualize data in Data-Forge Notebook."
   }
 ];
