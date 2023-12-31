43736265c605da40eaec9eadcae55cec45dfbb7c
Ashley Davis <ashley@codecapers.com.au>
2023-12-31 10:51:11 +1000

    Reproduced issue with error message in the testbed.

--- shells/testbed-cli/src/index.ts
+++ shells/testbed-cli/src/index.ts
@@ -1,3 +1,4 @@
+import { CodeEvaluator } from "evaluation-engine";
 import { JavaScriptCodeGenerator } from "evaluation-engine/build/lib/javascript-code-generator";
 
 async function main() {
@@ -11,12 +12,35 @@ async function main() {
             }
         ]
     };
-    const mockLog: any = {};
 
-    const codeGenerator = new JavaScriptCodeGenerator(notebook, "test-path", mockLog);
-    const code = await codeGenerator.genCode(notebook.cells);
+    const mockNpm: any = {
+        ensureRequiredModules() {
 
-    console.log(JSON.stringify(code, null, 4));
+        },
+    };
+
+    const mockLog: any = {
+        error(message: string, ...args: any[]) {
+            console.error(message, ...args);
+        },
+
+        info(message: string, ...args: any[]) {
+            console.log(message, ...args);
+        },
+        
+    };
+
+    // const codeGenerator = new JavaScriptCodeGenerator(notebook, "test-path", mockLog);
+    // const code = await codeGenerator.genCode(notebook.cells);
+
+    // console.log(JSON.stringify(code, null, 4));
+
+    // for (const diagnostic of code.diagnostics) {
+    //     this.reportError(ErrorSource.Compiler, this.getCurCellId(), diagnostic.message, diagnostic.location, undefined);
+    // }
+
+    const codeEvaluator = new CodeEvaluator(process, notebook, notebook.cells, "test-notebook", "c://temp", mockNpm, mockLog);
+    await codeEvaluator.evalCode();
 }
 
 main()
